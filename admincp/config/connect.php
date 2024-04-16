f2617c8d3072d132f6dedb87cf93282f2f9aae1f
kakaTran <tranvuca213@gmail.com>
2024-04-17 01:15:17 +0700

    conmmit

--- admincp/config/connect.php
+++ admincp/config/connect.php
@@ -1,7 +1,7 @@
 <?php
     global $mysqli; // set $mysql thanh global de o dau minh cung co the goi no
 
-    $mysqli = new mysqli("localhost","root","","shoeheaven");
+    $mysqli = new mysqli("103.255.237.2","webdevel_catv","a,1Tz[9]5tE;","webdevel_catv");
 
     // Check connection
     if ($mysqli -> connect_errno) {
